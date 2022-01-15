4e29f0a123f2909d4945dd2b9feb704ebcf4e8bb
DrDano <danielharned@gmail.com>
2022-01-14 17:46:23 -0800

    adding test Employee

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -4,9 +4,10 @@ describe('Employee Tests', () => {
     test('creates an employee object', () => {
         const employee = new Employee('Russ Hanneman', 15566, 'russ@gmail.com');
 
-        expect(employee.getName()).toBe('Russ Hanneman');
-        expect(employee.getId()).toEqual(expect.any(Number));
-        expect(employee.getEmail()).toEqual(expect.any(String));
+        expect(employee.getEmployeeInfo()).toEqual(
+        {"name": "Russ Hanneman", 
+        "id": 15566, 
+        "email": "russ@gmail.com"});
         expect(employee.getRole()).toEqual('Employee');  
     })
 });
